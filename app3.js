c946856610dfd4624aeb4afd24c49f1348b1752a
Pedro Henrique Pinheiro de Oliveira <pedrocaseis@hotmail.com>
2024-06-20 12:11:13 -0300

    Add files via upload

---
+++ app3.js
@@ -0,0 +1,25 @@
+var num = prompt("Digite um numero de 1 até o 7")
+
+switch (num) {
+    case "1":
+        console.log("Segunda")
+        break
+    case "2": 
+        console.log("Terça")
+        break
+    case "3":
+        console.log("Quarta")
+        break
+    case "4":
+        console.log("Quinta, quase sexta ein")
+        break
+    case "5":
+        console.log("SEXTOU ME FI, CUIDA")
+        break
+    case "6":
+        console.log("SABADOU, ressaca ta forte beber de novo para curar")
+        break
+    case "7":
+        console.log("Domingo do senhor")
+        break
+}
